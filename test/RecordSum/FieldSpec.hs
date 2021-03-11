6b553fcf90327e98a766baff727731d53a1db97f
Lukasz Golebiewski <lukasz.golebiewski@gmail.com>
2021-03-11 11:59:09 +0100

    Add basic test

--- test/RecordSum/FieldSpec.hs
+++ test/RecordSum/FieldSpec.hs
@@ -1,9 +1,31 @@
 module RecordSum.FieldSpec where
 
+import           Prelude
+
+import           Control.Lens.Combinators (view)
+import           GHC.Generics             (Generic)
 import           Hedgehog
+import           RecordSum.Field          (recordSumField)
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
 
-prop_1 :: Property
-prop_1 = property $ True === True
+data T1
+  = MkT11 D1
+  | MkT12 D2
+  deriving stock (Generic)
+
+data D1 = D1 {
+  dField :: Int
+} deriving stock (Generic)
+
+data D2 = D2 {
+  dField :: Int
+} deriving stock (Generic)
+
+prop_can_access_1 :: Property
+prop_can_access_1 = property $ do
+  let
+    fVal = 1
+    record = MkT11 $ D1 fVal
+  view (recordSumField @"dField") record === fVal
